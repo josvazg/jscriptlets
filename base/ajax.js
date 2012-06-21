bb499167f67eaf576a61703630586ab7bf93513b
Jose Luis Vázquez González <josvazg@gmail.com>
2012-06-21 10:21:28 +0200

    Fixed ajax.js, it was using prototypes code

--- base/ajax.js
+++ base/ajax.js
@@ -2,13 +2,26 @@
 // Tested on: IE, Chrome, Mozilla
 // Depends on:
 
+// Returns the first valid funtion
+function validFuncOf() {
+    var returnValue;
+    for (var i = 0, length = arguments.length; i < length; i++) {
+      var lambda = arguments[i];
+      try {
+        returnValue = lambda();
+        break;
+      } catch (e) { }
+    }
+    return returnValue;
+}
+
 // Obtains correct Ajax transport
 function getAjaxTransport() {
-    return Try.these(
+	return validFuncOf(
       function() {return new XMLHttpRequest()},
       function() {return new ActiveXObject('Msxml2.XMLHTTP')},
       function() {return new ActiveXObject('Microsoft.XMLHTTP')}
-    ) || false;
+    ) 
 }
 
 // Do an ajax GET or POST (if 'post' is not null)
